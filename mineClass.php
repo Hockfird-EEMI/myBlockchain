f6795fd54bfa5461ca4509dd8b31d72c020733dd
gsayer <gsayer@mcra.fr>
2018-11-09 16:04:03 +0100

    Ajout de commentaires

--- mineClass.php
+++ mineClass.php
@@ -16,7 +16,6 @@ class Chaine {
     $this->diff = rand(100000,500000);
     $this->dataHash = null;
   }
-
   public function setData($data) {
     $this->data = $data;
   }
@@ -41,6 +40,9 @@ class Chaine {
 
   public function hashBlock() {
     // Création de variable de difficulté en plus
+    // Je génère aléatoirement un chiffre entre 100.000 & 500.000
+    // Depuis ce chiffre je créer une string aléatoire de ce même nombre.
+    // Je hash mon bloc avec cette string
     $string = "";
     for ($i=0; $i < $this->diff; $i++) {
       $random_position = rand(0,strlen($string)-1);
@@ -49,11 +51,11 @@ class Chaine {
       $string = substr($string,0,$random_position).$random_char.substr($string,$random_position);
     }
 
-    if ($this->prev == null) {
+    if ($this->prev == null) { // Si cela est le premier fichier, je le hash sans le previous (Vu qu'il n'y en a pas)
       $this->dataHash = hash('sha256', $this->data . implode(getdate()) . $string);
     } else {
       $keyToCheck = $this->checkSum($this->prev->dataHash);
-      if ($keyToCheck == $_ENV["sumToFind"]) {
+      if ($keyToCheck == $_ENV["sumToFind"]) { // Je vérifie si ma clé checkSum est la même que j'ai gardé en variable d'environnement.
         $this->dataHash = hash('sha256', $this->data . $this->prev->dataHash . implode(getdate()) . $string);
       } else {
         var_dump('MIDDLE MAN ! WRONG FILE !');
@@ -63,14 +65,14 @@ class Chaine {
   }
 
   public function checkSum($dataHash) {
-    // Nous nous sommes mis d'accord sur le chiffre 9
+    // Nous nous sommes mis d'accord sur le chiffre 9 pour appliquer le modulo sur la somme de mon hash
     $arrayHash = str_split($dataHash);
     $sumHash = array_sum($arrayHash);
     $keyVal = ($sumHash%9);
-    return $keyVal;
+    return $keyVal; // Je stock ma clé checkSum en Variable d'Environnement
   }
 
-  public function show() {
+  public function show() { // Simple méthode show pour afficher "Proprement le contenu de mon bloc"
     var_dump("Name : ".$this->data);
     if ($this->prev == null) {var_dump("Prev : ".$this->prev);} else {var_dump("Prev : ".$this->prev->data);}
     if ($this->next == null) {var_dump("Next : ".$this->next);} else {var_dump("Next : ".$this->next->data);}
@@ -80,7 +82,7 @@ class Chaine {
     var_dump("--------------------------------------------------------");
   }
 
-  static function test() {
+  static function test() { // Function test a utiliser pour générer et miner 4 blocs
     $first = new Chaine("1st Block");
     $second = new Chaine("2nd Block");
     $third = new Chaine("3rd Block");
